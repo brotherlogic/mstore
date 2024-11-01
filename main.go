f85ff9864acfdc3e92624ce15d320fd4fa55ef87
Simon Tucker <brotherlogic@gmail.com>
2024-11-01 10:02:53 -0700

    Bounce

--- main.go
+++ main.go
@@ -83,13 +83,8 @@ func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
 
-	credential := options.Credential{
-		AuthSource: "admin",
-		Username:   "admin",
-		Password:   "pass",
-	}
-	clientOpts := options.Client().ApplyURI(*mongoAddress).
-		SetAuth(credential)
+	log.Printf("Logging in: %v", *mongoAddress)
+	clientOpts := options.Client().ApplyURI(*mongoAddress)
 	mclient, err := mongo.Connect(ctx, clientOpts)
 	defer func() {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
